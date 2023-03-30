3a6731d71931fa455a1a1a7dd8a730a669fbb17f
codepanther <pezechigozirim@gmail.com>
2023-03-30 17:20:55 +0100

    Add image assets

---
+++ src/constants/images.js
@@ -0,0 +1,29 @@
+import arrowOutward from '../assets/arrow_outward.png';
+import figma from '../assets/figma.png';
+import hero from '../assets/hero.png';
+import idea from '../assets/idea.png';
+import vector from '../assets/vector.png';
+import vscode from '../assets/vscode.png';
+import wordpress from '../assets/wordpress.png';
+import person1 from '../assets/person1.png';
+import person2 from '../assets/person2.png';
+import person3 from '../assets/person3.png';
+import person4 from '../assets/person4.png';
+import person5 from '../assets/person5.png';
+
+const images = {
+  arrowOutward,
+  figma,
+  hero,
+  idea,
+  vector,
+  vscode,
+  wordpress,
+  person1,
+  person2,
+  person3,
+  person4,
+  person5,
+};
+
+export default images;
