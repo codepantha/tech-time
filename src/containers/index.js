5ff091805fc5ed705f70fc266ebc0b780351b1a3
codepanther <pezechigozirim@gmail.com>
2023-03-31 16:29:05 +0100

    Create and style FAQs component

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,3 +3,4 @@ export { default as About } from './About';
 export { default as Courses } from './Courses';
 export { default as Testimonials } from './Testimonials';
 export { default as Community } from './Community';
+export { default as FAQs } from './FAQs';
